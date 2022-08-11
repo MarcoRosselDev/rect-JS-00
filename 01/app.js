84a570e9247df7f402abb703063a0b09f91271bb
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-11 13:33:59 -0400

    class 1.4

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,18 @@
-const h1 = document.querySelector('.putText');
-h1.innerHTML = `me gusta el pastel de pinna`;
+/* challeng 2
+
+1- create a variable, myAge, and set its value to your age.
+2- log the myAge variable to the console.
+*/
+
+const h1 = document.querySelector('.h1');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(){
+})
+
+/* 
+<form action="form">
+  <h3 class="h1">what's your name?</h3>
+  <input type="text">
+  <button type="submit">send</button>
+</form> */
