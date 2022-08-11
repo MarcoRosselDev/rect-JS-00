f317ea283edc07387cac8730585b6691f49c18bb
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-11 14:13:41 -0400

    class 2.3

--- 04-button-increment/app.js
+++ 04-button-increment/app.js
@@ -3,3 +3,7 @@ initialize the count as 0
 listen for clicks on the increment button
 increment the count varioable when the button is clicked
 change the count-el in the HTML to reflect the new count */
+
+function increment(){
+  console.log('the button was clicked');
+}
