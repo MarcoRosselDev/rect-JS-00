71b22852e3037e69f53d002c8b91bf6d2cab5f4d
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-12 11:15:39 -0400

    class 8

---
+++ 08-varibale-name/index.js
@@ -0,0 +1,15 @@
+// grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+// Create two variables (name and  greeting) that contains your name and the greeting we want to render on the page
+
+// Render the welcome message using welcomeEl.innerText
+
+/* 
+
+<form action="">
+  <h1 class="h1">What's your name</h1>
+  <input type="text">
+  <button type="submit">send</button>
+</form>
+
+*/
