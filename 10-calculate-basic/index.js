1eddca92a9b2fe781ddb595c0dc35a02b17a03c0
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-12 17:35:25 -0400

    class 10.4

--- 10-calculate-basic/index.js
+++ 10-calculate-basic/index.js
@@ -2,3 +2,12 @@ const addition = document.querySelector('.addition');
 const subtraction = document.querySelector('.subtraction');
 const multiplication = document.querySelector('.multiplication');
 const division = document.querySelector('.division');
+const h2 = document.querySelector('.h2');
+
+let num1 = 7;
+let num2 = 3;
+
+addition.addEventListener('click', function() {
+  const solution = num1 + num2;
+  h2.textContent += `[${solution}]`;
+})
