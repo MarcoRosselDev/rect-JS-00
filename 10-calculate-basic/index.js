8023542ae5b5180dde2b4c36bb47d3dd2c89a8aa
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-12 17:30:48 -0400

    class 10.3

---
+++ 10-calculate-basic/index.js
@@ -0,0 +1,4 @@
+const addition = document.querySelector('.addition');
+const subtraction = document.querySelector('.subtraction');
+const multiplication = document.querySelector('.multiplication');
+const division = document.querySelector('.division');
