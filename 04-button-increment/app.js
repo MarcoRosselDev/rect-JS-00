32aa052e873bf0e042fba89e838d7edb82d6ed09
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-12 08:26:21 -0400

    class 2.4

--- 04-button-increment/app.js
+++ 04-button-increment/app.js
@@ -4,6 +4,12 @@ listen for clicks on the increment button
 increment the count varioable when the button is clicked
 change the count-el in the HTML to reflect the new count */
 
-function increment(){
+/* function increment(){
   console.log('the button was clicked');
-}
+} */
+
+const up = document.querySelector('.increment-btn');
+
+up.addEventListener('click', function(){
+  console.log('you are clicked this button');
+})
