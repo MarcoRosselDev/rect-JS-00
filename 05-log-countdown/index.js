f323b808606eb63172d21b35cfa9a8d4cdc88a96
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-12 08:39:20 -0400

    class 3.3

--- 05-log-countdown/index.js
+++ 05-log-countdown/index.js
@@ -1,3 +1,8 @@
 // Create a function (you decide the name) that logs out the numbre 42 to the console
 // Call/invoke the function
 
+const button = document.querySelector('.countdown');
+
+button.addEventListener('click', function(){
+  
+})
