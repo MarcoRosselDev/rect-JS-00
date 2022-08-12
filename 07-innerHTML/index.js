11cca264978c7b2a8c2152f1ad153bc0aa9288ed
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-12 09:21:57 -0400

    class 5.2

--- 07-innerHTML/index.js
+++ 07-innerHTML/index.js
@@ -2,3 +2,12 @@
 
 //change the count-el in the html to reflect the new count
 
+let h1 = document.querySelector('.h1');
+const button = document.querySelector('.button');
+
+let count = 0;
+
+button.addEventListener('click', function(){
+  count = count + 1;
+  h1.innerText = count;
+})
