89de478c4e79e30759f1e592a8ab6fda3ea90337
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-13 11:08:33 -0400

    class 12.4

---
+++ 12-black-jack/index.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('.button');
+
+button.addEventListener('click', function(){
+
+})
+
+
+const cards = document.querySelectorAll('.cards');
+const sum = document.querySelectorAll('.sum');
+/* 
+  <h1>Blackjack</h1>
+  <p id="message-el">Want to play a round?</p>
+  <p class="cards">Cards:</p>
+  <p class="sum">Sum:</p>
+  <button class="button">START GAME</button>
+*/
