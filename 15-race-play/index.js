9698c6d5cd2f0ab39d02e96ecebec39b678f4d30
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-13 18:30:13 -0400

    class 15.2

--- 15-race-play/index.js
+++ 15-race-play/index.js
@@ -7,4 +7,15 @@
   <h3 class="h3"></h3>
 
 */
+const button = document.querySelector('.button');
 
+let player1 = Math.floor(Math.random()* 90);
+let player2 = Math.floor(Math.random()* 90);
+
+button.addEventListener('click', function(){
+  if (player1 > player2) {
+    
+  } else {
+    
+  }
+})
