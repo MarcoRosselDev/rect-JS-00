6e834c17adae887c0119b87dc7e3b17ced0f73d1
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-13 14:53:01 -0400

    class 12.11

--- 12-black-jack/index.js
+++ 12-black-jack/index.js
@@ -10,13 +10,26 @@ button.addEventListener('click', function(){
   /* reandom cards 1-11 */  
   let card1 = Math.floor(Math.random() * 12);
   let card2 = Math.floor(Math.random() * 12);
-  cards.innerText = `Cards: ${card1}, ${card2}`;
+  let a = cards.innerText = `Cards: ${card1}, ${card2}`;
   
   /* sum of the cards*/
   let sumCards = card1 + card2;
-  sum.innerText = `Sum: ${sumCards}`;
+  let b = sum.innerText = `Sum: ${sumCards}`;
   
   /* conditional and print text */
+  conditional(sumCards);
+  
+  buttonNewCard.addEventListener('click', function(){
+    let card3 = Math.floor(Math.random() * 12);
+    const sumAllCards = sumCards + card3;
+    a = cards.innerText = `Cards: ${card1}, ${card2}, ${card3}`;
+    sumCards = card1 + card2 + card3;
+    b = sum.innerText = `Sum: ${sumCards}`;
+    conditional(sumCards);
+  })
+})
+
+function conditional(sumCards){
   if (sumCards < 21) {
     h3.innerText = `Do you want to draw a new card?`;
     div.classList.remove('add');
@@ -25,14 +38,7 @@ button.addEventListener('click', function(){
   } else {
     h3.innerText = `You're out of the game!`;
   }
-  
-  buttonNewCard.addEventListener('click', function(){
-    let card3 = Math.floor(Math.random() * 12);
-    console.log(card3);
-    const sumAllCards = sumCards + card3;
-    console.log(sumAllCards); 
-  })
-})
+}
 
 
 
