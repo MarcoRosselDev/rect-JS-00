b4252595c4ac1e9741cbd7623633910406d4cf21
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-13 16:48:18 -0400

    class 14

--- 14-render-array/index.js
+++ 14-render-array/index.js
@@ -3,7 +3,5 @@ const h1 = document.querySelector('.h1');
 let array = ['hello', 'mi', 'name', 'is', 'marco', 'and', "I'm"];
 
 for (let i = 0; i < array.length; i++) {
-  const element = array[i];
-  console.table(i);
-  h1.innerHTML += array[i];
+  h1.textContent += array[i] + " ";
 }
