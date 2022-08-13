237e37b7bde3b0280a6512a8a4121b021967611a
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-13 15:59:24 -0400

    class 13.2

--- 13-arrays/index.js
+++ 13-arrays/index.js
@@ -1,5 +1,17 @@
-let number = [1, 2, 3, 4, 5];
+/* let number = [1, 2, 3, 4, 5];
 console.log(number);
 number.push(6);
 console.log(number);
 console.table(number);
+
+console.table(number[1]); */
+
+
+
+/* Create a for loop that counts from 10 to 100 in steps of 10
+
+Use console.log to log out the numbers*/
+
+for (let index = 0; index <= 100; index += 10) {
+  console.log(index);
+}
