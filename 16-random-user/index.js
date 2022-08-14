f35aad06ee123ab859d6cddb9618e80c05af0382
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-14 19:28:55 -0400

    class 16.5

--- 16-random-user/index.js
+++ 16-random-user/index.js
@@ -1,12 +1,3 @@
-/* 
-  <h1>Random user</h1>
-  <h2 class="name">Marco</h2>
-  <h3 class="age">29</h3>
-  <h3 class="nationality">chilean</h3>
-  <h3 class="phrase">I like architecture</h3>
-  <button class="button">random</button>
-*/
-
 const random = {
   1:{
     name: 'andres',
@@ -57,3 +48,13 @@ const age = document.querySelector('.age');
 const nationality = document.querySelector('.nationality');
 const phrase = document.querySelector('.phrase');
 const button = document.querySelector('.button');
+
+button.addEventListener('click', function() { 
+  randomUser();
+  
+})
+
+function randomUser(){
+  let user = Math.floor((Math.random() * 7) + 1);
+  return user;
+}
