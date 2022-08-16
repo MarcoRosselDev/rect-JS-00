7f147132d26eb1d72097e5b617d6537808f4adb3
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-16 18:44:26 -0400

    exercice 19.5

--- 19-exercises/2.js
+++ 19-exercises/2.js
@@ -1,5 +1,3 @@
-let age = 15;
-
 // lees than 6 years old -> free
 // 6 to 17 years old -> child discount
 // 18 to 26 years old  -> student discount
@@ -9,10 +7,20 @@ let age = 15;
 // create a conditional statement (if/else/else if) that logs out the discount
 // the passenger will get based upon the value of the age variable
 
+let a = 29;
+
 function passenger(age){
   if (age <= 6) {
     console.log('free');
-  } else if (age > 6 || age <= 17 ) {
+  } else if (age > 6 || age <= 17) {
     console.log('child discount');
-  } else if (age )
+  } else if (age > 17 || age <= 26) {
+    console.log('student discount');
+  } else if (age > 26 || age <= 66) {
+    console.log('full price');
+  } else {
+    console.log('senior citizen discount');
+  }
 }
+
+passenger(a);
