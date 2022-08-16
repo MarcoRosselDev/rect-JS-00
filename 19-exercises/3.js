e3ad4d17df8c284675bfcce2d5028fcd84d06ab0
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-16 19:04:05 -0400

    exercice 19.9

--- 19-exercises/3.js
+++ 19-exercises/3.js
@@ -10,3 +10,7 @@ the 5 largest countries in the world:
 - pakistan
 
 */
+console.log('the 5 largest countries in the world:');  
+for (let i = 0; i < largeCountries.length; i++) {
+  console.log( "- " + largeCountries[i]);
+}
