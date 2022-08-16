56fde6de664183dbb7c95ddedb2181e4703717e5
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-16 18:56:10 -0400

    exercice 19.8

---
+++ 19-exercises/3.js
@@ -0,0 +1,12 @@
+let largeCountries = ['china', 'india', 'usa', 'indonesia', 'pakistan'];
+
+/* use a for loop to log the following to the console:
+
+the 5 largest countries in the world:
+- china
+- india
+- united states
+- indonesia
+- pakistan
+
+*/
