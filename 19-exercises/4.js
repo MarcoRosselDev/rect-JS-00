5b42c2144ae4c612c0c1ad306cc65c408f864e4b
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-16 19:11:06 -0400

    exercice 19.10

---
+++ 19-exercises/4.js
@@ -0,0 +1,8 @@
+let largeCountries = ['tuvalu', 'india', 'usa', 'indonesia', 'monaco'];
+
+// you need to help me fixup the largeCountries array so that 
+// china and pakistan are added back into their respective places
+
+// use push and pop and their counterparts unshift() and shift()
+// google how to use unshift() and shift()
+
