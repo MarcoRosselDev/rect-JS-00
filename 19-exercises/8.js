1fd994a674362b48bfca3cf9e085003a97dda809
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-16 22:35:36 -0400

    exercice 19.7.10

---
+++ 19-exercises/8.js
@@ -0,0 +1,4 @@
+let fruit = ['🍊', '🍎', '🍊', '🍎', '🍎', '🍊'];
+
+// crate a function that puts the apples onto the appleShelf and the oranges onto the orangeShelf.
+// use a for loop, a conditional statement, and the textContent property.
