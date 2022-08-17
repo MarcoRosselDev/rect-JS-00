fca8ec44102d06a43e5513adf53dbad80b838bfc
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-16 22:20:27 -0400

    exercice 19.7.8

--- 19-exercises/7.js
+++ 19-exercises/7.js
@@ -1,3 +1,10 @@
 let hands = ['rock', 'paper', 'scissor'];
 
 // create a function that returns a random item from the array.
+
+function random() {
+}
+let a = Math.floor(Math.random * hands.length);
+console.log(a);
+
+console.log(hands.length);
