50b0b796b79ff4ff834e041c0a044f6b9d766cf4
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-17 19:38:57 -0400

    exercice 20.2

--- 20-chrom-extention/index.js
+++ 20-chrom-extention/index.js
@@ -1,1 +1,11 @@
-//
+/* 
+<input type="text" id="input-el">
+  <button id="input-btn">SAVE INPUT</button>
+*/
+
+const button = document.querySelector('.input-btn');
+const input = document.querySelector('.input-el');
+
+button.addEventListener('click', function() {
+  console.log('button is clicked');
+});
