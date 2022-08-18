d036395d9af7baad501bb3e913690dfe461f43b9
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-17 23:47:36 -0400

    exercice 21.2

---
+++ 21-chrom-extention-up/index.js
@@ -0,0 +1,10 @@
+/*
+<h1>list buy</h1>
+  <input type="text" class="input">
+  <button class="button">buy</button>
+  <div class="list"></div>
+*/
+
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+const list = document.querySelector('.list');
