ca1ed84b54d8ee8dcccd5490db2edc60c302df1f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-18 14:23:20 -0400

    exercice 22.7

--- 22-local-storage/1.js
+++ 22-local-storage/1.js
@@ -1,5 +1,6 @@
 let myLeads = `["www.awesomelead.com"]`;
 console.log(typeof(myLeads));
+console.log(myLeads);
 // 1. Turn the myLeads string into an array
 myLeads = JSON.parse(myLeads);
 console.log(typeof(myLeads));
@@ -7,5 +8,7 @@ console.log(typeof(myLeads));
 myLeads.push('www.example.com');
 console.log(myLeads);
 // 3. Turn the array into a string again
-
+myLeads = JSON.stringify(myLeads);
 // 4. Console.log the string using typeof to varify that it's a string
+console.log(typeof(myLeads));
+console.log(myLeads);
