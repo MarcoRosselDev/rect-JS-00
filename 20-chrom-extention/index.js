8eaa6cbb4b331337d2642581ce38504cc22c933f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-17 22:59:57 -0400

    exercice 20.8

--- 20-chrom-extention/index.js
+++ 20-chrom-extention/index.js
@@ -5,7 +5,7 @@
 
 // push the value "www.awesomelead.com" to myLeads when the input button is clicked
 
-let myLeads = [];
+let myLeads = ["a", "b", "c"];
 
 const input = document.querySelector('.input-el');
 const button = document.querySelector('.input-btn');
@@ -14,5 +14,15 @@ const ul = document.querySelector('.ul');
 
 button.addEventListener('click', function() {
   myLeads.push(input.value);
-  ul.innerHTML += "<li>" + input.value + "</li> <br>";
+  ul.innerHTML += "<li>" + input.value + "</li>";
+  //si uso innerText se renderia el li, para eso es el innerHTML y sus diferencias
+
 });
+
+for (let i = 0; i < myLeads.length; i++) {
+  const li = document.createElement('li');
+  li.textContent = myLeads[i];
+  ul.append(li);
+}
+
+//esto hace lo mismo pero es mucho mas entendible la primero por que es mas visual
