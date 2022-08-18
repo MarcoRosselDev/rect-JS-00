63a1b913a6da026781de499ad2f8ea4bb7af6941
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-18 14:15:23 -0400

    exercice 22.4

---
+++ 22-local-storage/1.js
@@ -0,0 +1,4 @@
+// 1. Turn the myLeads string into an array
+// 2. Push a new value to the array
+// 3. Turn the array into a string again
+// 4. Console.log the string using typeof to varify that it's a string
