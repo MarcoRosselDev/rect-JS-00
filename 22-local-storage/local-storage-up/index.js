c6bc959f96b41d241bfd7bbf9e9a94937784029f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-18 14:37:10 -0400

    exercice 22.7.2

---
+++ 22-local-storage/local-storage-up/index.js
@@ -0,0 +1,8 @@
+/* <h2>list to do</h2>
+  <input type="text" class="input">
+  <button class="button">put</button>
+  <h4 class="h4"></h4> */
+
+  const input = document.querySelector('.input');
+  const button = document.querySelector('.button');
+  const h4 = document.querySelector('.h4');
