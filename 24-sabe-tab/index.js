3d65a48b7ee41edef6fe40920c379c78e5a268bc
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-18 22:31:27 -0400

    exercice 24.3

--- 24-sabe-tab/index.js
+++ 24-sabe-tab/index.js
@@ -2,7 +2,17 @@ const inpt = document.querySelector('.inpt');
 const put = document.querySelector('.put');
 const saveTab = document.querySelector('.save-tab');
 const cleanAll = document.querySelector('.clean-all');
+const ul = document.querySelector('.ul');
 
 put.addEventListener('click', function(){
-  
-})
+  printAll(inpt.value);
+});
+
+function printAll(link) { 
+  ul.innerHTML += `
+  <li>
+      ${link}
+  </li>
+  `
+  inpt.value = "";
+}
