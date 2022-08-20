4948e42bcc84fc47c2d879c398a70a4e6ad83509
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-20 17:40:17 -0400

    exercice 24.4.2

--- 24-sabe-tab/+/index.js
+++ 24-sabe-tab/+/index.js
@@ -1,12 +1,10 @@
-/*
-  <h1>list</h1>
-  <input type="text" class="ipt">
-  <button class="btn-put">put</button>
-  <button class="btn-clean">clean</button>
-  <div class="li"></div>
-*/
-
 const ipt = document.querySelector('.ipt');
 const btnPut = document.querySelector('.btn-put');
 const btnClean = document.querySelector('.btn-clean');
 const li = document.querySelector('.li');
+let saveArray = [];
+
+btnPut.addEventListener('click', function() {
+  saveArray.push(ipt.value);
+  console.log(saveArray);
+})
