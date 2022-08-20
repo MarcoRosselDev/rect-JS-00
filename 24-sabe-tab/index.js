0c13d20c7edab0a222d0bdc70a5a8b97462c0228
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-19 22:59:10 -0400

    exercice 24.2.4

--- 24-sabe-tab/index.js
+++ 24-sabe-tab/index.js
@@ -3,24 +3,30 @@ const put = document.querySelector('.put');
 const saveTab = document.querySelector('.save-tab');
 const cleanAll = document.querySelector('.clean-all');
 const ul = document.querySelector('.ul');
+const localStr = JSON.parse(localStorage.getItem("listExample"));
+//const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")); 
 let link = [];
 
-put.addEventListener('click', function(){
-  link.push(inpt.value);
-  inpt.value = "";
+if (localStr) {
+  link = localStr;
   prinnt(link);
-})
+}
 
 function prinnt(arr) {
   let it = "";
   for (let i = 0; i < arr.length; i++) {
     it += `
     <li>
-      ${arr[i]}
+    ${arr[i]}
     </li>
     `
   }
-
-
   ul.innerHTML = it;
 }
+
+put.addEventListener('click', function(){
+  link.push(inpt.value);
+  inpt.value = "";
+  localStorage.setItem('listExample', JSON.stringify(link));
+  prinnt(link);
+})
