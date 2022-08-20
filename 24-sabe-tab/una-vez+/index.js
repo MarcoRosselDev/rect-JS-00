3e2b810a12a4c9cd151d1515f2beb06e431b60a4
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-19 23:13:26 -0400

    exercice 24.2.8

--- 24-sabe-tab/una-vez+/index.js
+++ 24-sabe-tab/una-vez+/index.js
@@ -5,3 +5,15 @@
   <button class="btn-clean">clean all</button>
   <div class="li"></div>
 */
+
+const inpt = document.querySelector('.inpt');
+const put = document.querySelector('.btn-put');
+const clean = document.querySelector('.btn-clean');
+const li = document.querySelector('.li');
+let parrafo = [];
+
+console.log(inpt, put, clean, li);
+
+put.addEventListener('click', function() {
+  
+})
