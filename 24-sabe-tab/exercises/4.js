93efbf806192b32b1af339806246bcd4cfcf413a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-20 19:55:53 -0400

    exercice 24.5.exercises.4.1

---
+++ 24-sabe-tab/exercises/4.js
@@ -0,0 +1,6 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+let a = localStorage.getItem('example');
+console.log(a);
