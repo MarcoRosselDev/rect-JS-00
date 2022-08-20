fea533bdd1421a99c7b367d54a03b79a11aebf9f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-20 17:36:15 -0400

    exercice 24.4.1

---
+++ 24-sabe-tab/+/index.js
@@ -0,0 +1,12 @@
+/*
+  <h1>list</h1>
+  <input type="text" class="ipt">
+  <button class="btn-put">put</button>
+  <button class="btn-clean">clean</button>
+  <div class="li"></div>
+*/
+
+const ipt = document.querySelector('.ipt');
+const btnPut = document.querySelector('.btn-put');
+const btnClean = document.querySelector('.btn-clean');
+const li = document.querySelector('.li');
