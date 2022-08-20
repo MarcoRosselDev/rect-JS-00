7529009f71cf8243d122f7e7f8971f879b5565b2
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-20 19:04:32 -0400

    exercice 24.5.exercises.1.2

--- 24-sabe-tab/exercises/1.js
+++ 24-sabe-tab/exercises/1.js
@@ -1,6 +1,13 @@
 // Create a function, getFirst(arr), that returns the fisrt item in the array
 
+const array = ['hi', 'mi', 'name', 'is'];
 
+function getFirst(arr) {
+  let first = arr[0];
+  return first;
+}
+
+console.log(getFirst(array));
 
 
 
