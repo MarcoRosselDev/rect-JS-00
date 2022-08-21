16f60bea8c31cecf7e3cde690d333ec39711a8bf
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-21 19:55:47 -0400

    exercice 24.5.exercises.8.10.2

--- 25-test-react/remaind-practis/i.js
+++ 25-test-react/remaind-practis/i.js
@@ -5,3 +5,7 @@
   <button class="btn-clean">clean</button>
   <div class="li"></div>
 */
+const inp = document.querySelector('.inp');
+const btnSubmit = document.querySelector('.btn-submit');
+const btnClean = document.querySelector('.btn-clean');
+const li = document.querySelector('.li');
