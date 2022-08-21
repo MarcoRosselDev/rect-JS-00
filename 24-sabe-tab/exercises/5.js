f76c5743394937363d29158921a3a33ae55ec82a
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-20 22:33:51 -0400

    exercice 24.5.exercises.5.2

--- 24-sabe-tab/exercises/5.js
+++ 24-sabe-tab/exercises/5.js
@@ -12,3 +12,5 @@ let data = [
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
+
+const btn = document.querySelector('.btn');
