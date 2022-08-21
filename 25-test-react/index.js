cb8d08f169cf0b365499369cb33d5949e554be2e
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-21 19:44:12 -0400

    exercice 24.5.exercises.8.9

--- 25-test-react/index.js
+++ 25-test-react/index.js
@@ -1,3 +1,8 @@
-import * as ReactDOM from 'react-dom';
-
 ReactDOM.render(<h1>hello, everyone!</h1>)
+
+
+const a = '3' + 4 + 5;  // "345"  and is a string
+const b = 3 + 4 + '5';  // "75" and is a string
+
+console.log(a, b);
+console.log(typeof(a), typeof(b));
