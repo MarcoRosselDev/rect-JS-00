e63c74412646ea97e1ffda61018e8c0a8ec44903
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-21 23:30:53 -0400

    26-exercise-1

---
+++ 26-exercise-math/01.js
@@ -0,0 +1,6 @@
+/*
+  <input type="text" class="ipt-cuadrado">
+  <button class="btn-area-cuadrado">area</button>
+  <button class="btn-perimetro-cuadrado">perimetro</button>
+  <div class="cuadrado"></div>
+*/
