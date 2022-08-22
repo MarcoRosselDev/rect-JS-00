7969aba552199c628a0ceed17c961a5f71af618f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-22 04:48:59 -0400

    26-exercise-1.9

--- 26-exercise-math/01.js
+++ 26-exercise-math/01.js
@@ -4,10 +4,19 @@ const btnPerimetro = document.querySelector('.btn-perimetro-cuadrado');
 const divCuadrado = document.querySelector('.cuadrado');
 
 btnArea.addEventListener('click', function(){
-  divCuadrado.innerHTML += `
-  <h4>The area of a square
-  white ${iptCuadrado.value} meters
-  is: <strong>${iptCuadrado.value ** 2} meters square</strong></h4>`
+  let printDOM = "";
+
+  if (iptCuadrado.value == 0) {
+    printDOM += `<p>please put a number other than 0</p>`
+  } else if (iptCuadrado.value < 0) {
+    printDOM += `<p class="pwarning">please put a positive number</p>`
+  } else {
+    printDOM += `
+    <h4>The area of a square
+    white ${iptCuadrado.value} meters
+    is: <strong>${iptCuadrado.value ** 2} meters square</strong></h4>`
+  }
+  divCuadrado.innerHTML += printDOM;
   iptCuadrado.value = "";
 });
 
