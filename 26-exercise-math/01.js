b5333800f58dcb495dae02897da1990700c629aa
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-21 23:50:29 -0400

    26-exercise-1.2

--- 26-exercise-math/01.js
+++ 26-exercise-math/01.js
@@ -1,6 +1,13 @@
-/*
-  <input type="text" class="ipt-cuadrado">
-  <button class="btn-area-cuadrado">area</button>
-  <button class="btn-perimetro-cuadrado">perimetro</button>
-  <div class="cuadrado"></div>
-*/
+const iptCuadrado = document.querySelector('.ipt-cuadrado');
+const btnArea = document.querySelector('.btn-area-cuadrado');
+const btnPerimetro = document.querySelector('.btn-perimetro-cuadrado');
+const divCuadrado = document.querySelector('.cuadrado');
+
+btnArea.addEventListener('click', function(){
+  if (iptCuadrado.value === string) {
+    console.log('put a number please');
+  } else {
+    console.log(iptCuadrado.value ** 2);
+  }
+  iptCuadrado.value = "";
+})
