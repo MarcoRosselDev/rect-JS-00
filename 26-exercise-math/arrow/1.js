74865bf345476b81fbeda55d1a46baf21b2a81ce
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-22 18:23:51 -0400

    26-exercise-2.2

--- 26-exercise-math/arrow/1.js
+++ 26-exercise-math/arrow/1.js
@@ -5,3 +5,24 @@
   <button class="btn-clean">clean</button>
   <div class="li"></div>
 */
+
+const ipt = document.querySelector('.ipt');
+const btnPush = document.querySelector('.btn-push');
+const btnClean = document.querySelector('.btn-clean');
+const li = document.querySelector('.li');
+let saveList = [];
+
+btnPush.addEventListener('click', function(){
+  saveList.push(ipt.value);
+  ipt.value = "";
+  localStorage.setItem('example', JSON.stringify(saveList));
+  printDOM(saveList);
+})
+
+const printDOM = (arr) => {
+  let printHTML = "";
+  for (let i = 0; i < arr.length; i++) {
+    printHTML += `<li>${arr[i]}</li>`
+  }
+  li.innerHTML = printHTML;
+}
