45fda3e9f742c4846c1ca9ee80df302bfcf0c678
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-21 20:10:29 -0400

    exercice 24.5.exercises.8.10.5

--- 25-test-react/remaind-practis/i.js
+++ 25-test-react/remaind-practis/i.js
@@ -1,15 +1,15 @@
-/*
-  <h1>list</h1>
-  <input type="text" class="inp">
-  <button class="btn-submit">click</button>
-  <button class="btn-clean">clean</button>
-  <div class="li"></div>
-*/
 const inp = document.querySelector('.inp');
 const btnSubmit = document.querySelector('.btn-submit');
 const btnClean = document.querySelector('.btn-clean');
 const li = document.querySelector('.li');
 let saveInput = [];
+
+const isInLocalStorage = JSON.parse(localStorage.getItem('exampleObject'));
+
+if (isInLocalStorage) {
+  saveInput = isInLocalStorage;
+  printDOM(saveInput);
+}
 
 btnSubmit.addEventListener('click', function(){
   saveInput.push(inp.value);
